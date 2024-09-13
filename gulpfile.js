0642ac9981f745f4f439a083698382fa141f52e2
Enio Silveira Leite <eniosleite@yahoo.com.br>
2024-09-13 16:11:01 -0300

    ajustando imagens na pasta dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,33 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
+//Função para compilar o Sass e mover o CSS para o dist
 function styles() {
-    return gulp.src('.\src\styles\*.scss')
+    return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('.\dist\css'));
+        .pipe(gulp.dest('./dist/css'));
 }
 
+
+//Função para otimizar imagens e mover para o dist
 function images() {
-    return gulp.src('.\src\images\**\*')
+    return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('.\dist\images'));
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles);
+//Função para copiar o JavaScript para o dist
+function scripts() {
+    return gulp.src('./src/scripts/**/*.js')
+        .pipe(gulp.dest('./dist/scripts'));
+}
 
+//Tarefa padrão que inclui a compilação do Sass, cópia de imagens e javaScript
+exports.default = gulp.parallel(styles, images, scripts);
+
+//Tarefa de watch que observa mudanças em arquivos .scss, imagens e scripts
 exports.watch = function() {
-    gulp.watch('.\src\styles\*.scss', gulp.parallel(styles, images))
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/images/**/*', images);
+    gulp.watch('./src/scripts/**/*.js', scripts);
 }
