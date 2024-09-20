e6bf435e023308c52bd0e81b307d4e5ddece3cfe
Enio Silveira Leite <eniosleite@yahoo.com.br>
2024-09-20 15:10:36 -0300

    criando configurando *.js

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ exports.default = gulp.parallel(styles, images, scripts);
 
 //Tarefa de watch que observa mudanças em arquivos .scss, imagens e scripts
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/images/**/*', gulp.parallel(images));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/images/**/*', gulp.parallel(images))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
